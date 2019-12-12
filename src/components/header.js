2dd7443d242cea9f6f9e58ad9df68843325fae71
Tim Brunette <timothybrunette@gmail.com>
2019-12-12 19:46:11 +1100

    add name

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,26 @@
 import PropTypes from "prop-types"
 import React from "react"
 import styled from "styled-components"
+import { Typography } from "@material-ui/core"
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 1rem;
+`
+const Title = styled.h1`
+  margin-bottom: 0;
 `
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <Wrapper>
-      <h1>{siteTitle}</h1>
-    </Wrapper>
-  </header>
+  <Wrapper>
+    <Title>{siteTitle}</Title>
+    <Typography variant='caption' align='center'>
+      developed by <a href='https://timothybrunette.com'>Timothy Brunette</a>
+    </Typography>
+  </Wrapper>
 )
 
 Header.propTypes = {
